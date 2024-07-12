b7d4ce9366ed5d7e0c4c801cb758e84d2752dd0f
Aiden Williams <aidenwilliams336@gmail.com>
2024-07-12 11:50:05 +0200

    added the Dots

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     let result = '';
 
+
     // Validation logic: Check if input starts with 'pet_' and is followed by alphanumeric characters
     let regex = /^pet_\w+$/;
 
     if (regex.test(input)) {
-        result = 'Valid Syntax';
+        result = 'Valid Syntax 🟢';
     } else {
-        result = 'Invalid Syntax';
+        result = 'Invalid Syntax 🔴';
     }
 
     document.getElementById('result').innerText = result;
